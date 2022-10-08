5c0af0b4dda2ef083f90760f5b8b46e67f6a4deb
Longok123 <115337273+Longok123@users.noreply.github.com>
2022-10-08 13:02:24 +0700

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-if (!$this->ketnoi)
-        {
-            $this->ketnoi = mysqli_connect('localhost', 'root', '', 'chanlemomo') or die ('Vui lòng kết nối đến DATABASE');
-            mysqli_query($this->ketnoi, "set names 'utf8'");
-        }
-    }
